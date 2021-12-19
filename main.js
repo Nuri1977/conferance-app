b51533807f731df963e9e8bff95a12a97b1fd144
Nuri1977 <nurilacka@gmail.com>
2021-12-19 17:24:05 +0100

    Add mobile view header navigation

---
+++ main.js
@@ -0,0 +1,32 @@
+const burger = document.querySelector('.burger');
+const nav1 = document.querySelector('.nav1');
+const nav2 = document.querySelector('.nav2');
+const btnCls = document.querySelector('.btn-close');
+const fb = document.querySelector('.fb');
+const tw = document.querySelector('.tw');
+const logo = document.querySelector('.logo');
+
+
+
+burger.addEventListener('click', () => {
+  burger.classList.add('hide');
+  nav1.classList.replace('hide', 'show');
+  nav2.classList.replace('hide', 'show');
+  btnCls.classList.replace('hide', 'show');
+  fb.classList.replace('show', 'hide');
+  tw.classList.replace('show', 'hide');
+  logo.classList.replace('show', 'hide');
+});
+
+btnCls.addEventListener('click', () => {
+  burger.classList.remove('hide');
+  nav1.classList.replace('show', 'hide');
+  nav2.classList.replace('show', 'hide');
+  btnCls.classList.replace('show', 'hide');
+  fb.classList.replace('hide', 'show');
+  tw.classList.replace('hide', 'show');
+})
+
+function test() {
+  console.log(nav2);
+}
